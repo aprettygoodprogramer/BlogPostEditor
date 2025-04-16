cc704d498a5f2710eff54e03602d6cde08f95503
Evan <evan.j.ducas@gmail.com>
2025-04-16 08:40:55 -0400

    Added the text box, and the markdown veiwer!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 use eframe::egui;
+use egui_commonmark::CommonMarkViewer;
 
-struct MyApp;
+struct MyApp {
+    my_string: String,
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Hello, world!");
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.vertical(|ui| {
+                        ui.add(egui::TextEdit::multiline(&mut self.my_string).desired_rows(10));
+                    });
+
+                    ui.vertical(|ui| {
+                        egui::ScrollArea::vertical().show(ui, |ui| {
+                            ui.label("Markdown Preview:");
+                            egui::ScrollArea::vertical().show(ui, |ui| {
+                                let mut cache = egui_commonmark::CommonMarkCache::default();
+                                CommonMarkViewer::new().show(ui, &mut cache, &self.my_string);
+                            });
+                        });
+                    });
+                });
+            });
         });
     }
 }
 
 fn main() {
     let options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", options, Box::new(|_cc| Ok(Box::new(MyApp))));
+    eframe::run_native(
+        "Markdown Previewer",
+        options,
+        Box::new(|_cc| {
+            Ok(Box::new(MyApp {
+                my_string: String::from("# Hello\n- Markdown\n- Preview"),
+            }))
+        }),
+    );
 }
